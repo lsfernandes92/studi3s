f504e34caf5b421409685f7bd7ba390475da8287
lsfernandes92 <lsfernandes92@gmail.com>
2019-10-21 12:08:31 -0700

    Remove submodule private-events

--- Ruby/Rails/private-events/app/models/user.rb
+++
@@ -1,7 +0,0 @@
-class User < ApplicationRecord
-  has_many :events
-
-  validates :name,
-    presence: true,
-    length: { maximum: 10 }
-end
--- Ruby/Rails/private-events/db/migrate/20190830223423_add_name_to_events.rb
+++
@@ -1,5 +0,0 @@
-class AddNameToEvents < ActiveRecord::Migration[5.2]
-  def change
-    add_column :events, :name, :string
-  end
-end
